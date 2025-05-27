4d390f0fb15d381ec24a8ceafc7f90c9667390a3
Ko-Wei Tai <kotaik@student-cluster.inf.ethz.ch>
2025-05-27 02:02:47 +0200

    Adjust train.sh and eval.sh

---
+++ eval.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --gpus=1
+#SBATCH --account digital_human
+#SBATCH --time=1:00:00
+#SBATCH --output=logs/resent50_surfnorm_eval_test.out
+
+python eval_regression_surfnorm_egobody.py --checkpoint /home/kotaik/DH/EgoDepth-HMR/tmp/75586/best_global_model.pt --dataset_root /work/courses/digital_human/13/egobody_release
--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 #SBATCH --gpus=1
 #SBATCH --account digital_human_jobs
 #SBATCH --time=8:00:00
-#SBATCH --output=logs/resent50_surfnorm.out
+#SBATCH --output=logs/resent50_surfnorm_2.out
 
-# python train_prohmr_surfnormals_egobody.py --load_pretrained true --checkpoint /home/weiwan/DigitalHuman/EgoDepth-HMR/tmp/16489/best_global_model.pt --log_step 250 
-python train_prohmr_surfnormals_egobody.py --log_step 366 --load_pretrained true --checkpoint /work/courses/digital_human/13/weiwan/surf_normals/best_global_model_surf.pt
+# python train_prohmr_surfnormals_egobody.py --log_step 366 --load_pretrained true --checkpoint /work/courses/digital_human/13/weiwan/surf_normals/best_global_model_surf.pt
+python train_prohmr_surfnormals_egobody.py --log_step 366 --load_pretrained true --checkpoint /home/kotaik/DH/EgoDepth-HMR/tmp/75586/best_global_model.pt
